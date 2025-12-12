11f871cfe3c44fd7932adfd55f34f1d96003f665
waltermichelraja <waltermichelraja@gmail.com>
2025-12-12 21:17:45 +0530

    LongestPlaindrome: subsequence()

--- algorithms/search/LongestPalindrome.java
+++ algorithms/search/LongestPalindrome.java
@@ -22,5 +22,34 @@ public class LongestPalindrome{
         }
         return s.substring(start,start+maxLen);
     }
+    public static String subsequence(String s){
+        int n=s.length();
+        int[][] dp=new int[n][n];
+        for(int i=0;i<n;i++)dp[i][i]=1;
+        for(int len=2;len<=n;len++){
+            for(int i=0;i<=n-len;i++){
+                int j=i+len-1;
+                if(s.charAt(i)==s.charAt(j)){
+                    dp[i][j]=dp[i+1][j-1]+2;
+                }else{
+                    dp[i][j]=Math.max(dp[i+1][j],dp[i][j-1]);
+                }
+            }
+        }
+        StringBuilder sb=new StringBuilder();
+        int i=0, j=n-1;
+        while(i<=j){
+            if(s.charAt(i)==s.charAt(j)){
+                sb.append(s.charAt(i));
+                i++; j--;
+            }
+            else if(dp[i+1][j]>=dp[i][j-1]){i++;}
+            else{j--;}
+        }
+        String half=sb.toString();
+        String rev=new StringBuilder(half).reverse().toString();
+        if(dp[0][n-1]%2==0)return half+rev;
+        return half.substring(0,half.length()-1)+rev;
+    }
 }
 

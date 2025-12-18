484de1f10d561c7517d35cfe22cea6302b504090
waltermichelraja <waltermichelraja@gmail.com>
2025-12-18 23:06:57 +0530

    Fibonacci: fiboTab()

--- algorithms/Fibonacci.java
+++ algorithms/Fibonacci.java
@@ -12,6 +12,13 @@ public class Fibonacci{
         memo[n]=fiboMemo(n-1)+fiboMemo(n-2);
         return memo[n];
     }
+    public static int fiboTab(int n) {
+        if(n<=1){return n;}
+        int[] dp=new int[n+1];
+        dp[0]=0; dp[1]=1;
+        for(int i=2;i<=n;i++){dp[i]=dp[i-1]+dp[i-2];}
+        return dp[n];
+    }
     public static int fibo(int n) {
         if(n<=1){return n;}
         return fibo(n-1)+fibo(n-2);

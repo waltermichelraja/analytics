e73150236a986533ba8f4aef774ba2774075420e
waltermichelraja <waltermichelraja@gmail.com>
2025-12-24 12:24:29 +0530

    BaseConversion: binaryToOctal(), binaryToOctalRec()

--- algorithms/BaseConversion.java
+++ algorithms/BaseConversion.java
@@ -14,6 +14,7 @@ public class BaseConversion{
         if(n==0){return 0;}
         return decimalToBinaryRec(n/2)*10+(n%2);
     }
+
     public static int binaryToDecimal(int bin){
         int dec=0;
         int base=1;
@@ -28,6 +29,35 @@ public class BaseConversion{
     public static int binaryToDecimalRec(int bin){
         if(bin==0){return 0;}
         return (bin%10)+2*binaryToDecimalRec(bin/10);
+    }
+
+    public static int binaryToOctal(int bin){
+        int oct=0;
+        int place=1;
+        while(bin>0){
+            int group=0;
+            int power=1;
+            for(int i=0;i<3&&bin>0;i++){
+                group+=(bin%10)*power;
+                power*=2;
+                bin/=10;
+            }
+            oct+=group*place;
+            place*=10;
+        }
+        return oct;
+    }
+    public static int binaryToOctalRec(int bin){
+        if(bin==0){return 0;}
+        int group=0;
+        int power=1;
+        int temp=bin;
+        for(int i=0;i<3&&temp>0;i++){
+            group+=(temp%10)*power;
+            power*=2;
+            temp/=10;
+        }
+        return binaryToOctalRec(bin/1000)*10+group;
     }
 }
 

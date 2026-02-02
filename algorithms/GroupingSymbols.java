a68e9b49accce725faed5103cc05a61e73d65646
waltermichelraja <waltermichelraja@gmail.com>
2026-02-02 18:15:57 +0530

    GroupingSymbols: firstInvalidIndex()

--- algorithms/GroupingSymbols.java
+++ algorithms/GroupingSymbols.java
@@ -54,5 +54,21 @@ public class GroupingSymbols{
         }
         return sb.toString();
     }
+
+    public static int firstInvalidIndex(String s){
+        Stack<Character> stack=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='('||ch=='{'||ch=='['){stack.push(ch);}
+            else if(ch==')'||ch=='}'||ch==']'){
+                if(stack.isEmpty())return i;
+                char top=stack.pop();
+                if((ch==')'&&top!='(')||(ch=='}'&&top!='{')||(ch==']'&&top!='[')){
+                    return i;
+                }
+            }
+        }
+        return stack.isEmpty()?-1:s.length();
+    }
 }
 

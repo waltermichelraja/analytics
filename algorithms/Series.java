f9a43d83a16e5e1847e92f0f2d7967af81a5a6bc
waltermichelraja <waltermichelraja@gmail.com>
2026-02-07 19:10:00 +0530

    Series: catalan()

--- algorithms/Series.java
+++ algorithms/Series.java
@@ -46,6 +46,15 @@ public class Series{
         }
         return dp[n-1];
     }
-
+    public static int catalan(int n){
+        int[] dp=new int[n+1];
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            for(int j=0;j<i;j++){
+                dp[i]+=dp[j]*dp[i-j-1];
+            }
+        }
+        return dp[n];
+    }
 }
 

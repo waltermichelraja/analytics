6de81eaf28b96895e69e769a090685a7d0373340
waltermichelraja <waltermichelraja@gmail.com>
2026-02-10 20:11:54 +0530

    Series: pell()

--- algorithms/Series.java
+++ algorithms/Series.java
@@ -67,5 +67,15 @@ public class Series{
         }
         return b;
     }
+    public static int pell(int n){
+        if(n<=1){return n;}
+        int a=0,b=1;
+        for(int i=2;i<=n;i++){
+            int c=2*b+a;
+            a=b;
+            b=c;
+        }
+        return b;
+    }
 }
 

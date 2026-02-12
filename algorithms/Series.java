d31ff2077cce227f7f471410ab09438b5e906bf0
waltermichelraja <waltermichelraja@gmail.com>
2026-02-12 18:26:39 +0530

    Series: isPerfect()

--- algorithms/Series.java
+++ algorithms/Series.java
@@ -103,5 +103,17 @@ public class Series{
         }
         return res;
     }
+
+    public static boolean isPerfect(long n){
+        if(n<2)return false;
+        long sum=1;
+        for(long i=2;i*i<=n;i++){
+            if(n%i==0){
+                sum+=i;
+                if(i!=n/i){sum+=n/i;}
+            }
+        }
+        return sum==n;
+    }
 }
 
